f99718e61862a058a22245e535901d09d34fa502
vamsi <vamsi.bommireddy@staples.com>
2026-02-14 12:43:55 -0500

    second commit to springboothello

---
+++ src/main/java/com/git/aks/GitToAksApplication.java
@@ -0,0 +1,13 @@
+package com.git.aks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitToAksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitToAksApplication.class, args);
+	}
+
+}
